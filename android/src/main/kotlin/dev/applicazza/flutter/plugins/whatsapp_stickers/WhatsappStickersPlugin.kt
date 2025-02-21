34bbf7034602c68631c9ae4879ce7c49265cb809
novikov <novikov.appstudio@gmail.com>
2025-02-22 02:05:21 +0300

    Migrate to Gradle DSL, Android v2 Embedding, Flutter 3.29

--- android/src/main/kotlin/dev/applicazza/flutter/plugins/whatsapp_stickers/WhatsappStickersPlugin.kt
+++ android/src/main/kotlin/dev/applicazza/flutter/plugins/whatsapp_stickers/WhatsappStickersPlugin.kt
@@ -15,7 +15,6 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 
 /** WhatsappStickersPlugin */
@@ -41,26 +40,11 @@ public class WhatsappStickersPlugin: FlutterPlugin, MethodCallHandler, ActivityA
     context = flutterPluginBinding.applicationContext
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
   companion object {
 
     private const val EXTRA_STICKER_PACK_ID = "sticker_pack_id"
     private const val EXTRA_STICKER_PACK_AUTHORITY = "sticker_pack_authority"
     private const val EXTRA_STICKER_PACK_NAME = "sticker_pack_name"
-
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "whatsapp_stickers")
-      channel.setMethodCallHandler(WhatsappStickersPlugin())
-    }
 
     @JvmStatic
     fun getContentProviderAuthorityURI(context: Context): Uri{
